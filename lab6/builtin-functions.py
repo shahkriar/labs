b509c6aeefb8cee276e729b6c994f85a50690ac8
shaho <shah.tanatov@mail.ru>
2024-03-04 23:35:54 +0500

    lab6 has been changed

--- lab6/builtin-functions.py
+++ lab6/builtin-functions.py
@@ -1,15 +1,15 @@
 import math
 import time
 
-#ex 1
+#ex 1 product of all numbers in the list
 def ex1():
     n = int(input("Input length: "))
-    list = []
+    lst = []
     for i in range(n):
-        list.append(int(input()))
-    print(math.prod(list))
+        lst.append(int(input()))# convert into int then add to empty list
+    print(math.prod(lst)) #finding product
 
-#ex 2
+#ex 2 number of upper and lower case letters
 def ex2():
     text = str(input("Input the text: "))
     low = sum(1 for letter in text if letter.islower())
@@ -18,7 +18,7 @@ def ex2():
     print('Lower case letters: ', low)
     print('Upper case letters: ', up)
 
-#ex 3
+#ex 3 is polindrome ?
 def ex3():
     text = str(input("Input the text: "))
     if text == text[::-1]:
@@ -26,18 +26,18 @@ def ex3():
     else:
         print(text, "is not a palindrome")
     
-#ex 4
+#ex 4 find sqrt root after certain miliseconds
 def ex4():
     num = int(input("Input the number: "))
     mil= int(input("Input the number of miliseconds: "))
-    seconds = mil/1000
+    seconds = mil/1000 #from miliseconds to seconds
     
-    time.sleep(seconds)
-    result = int(math.sqrt(num))
+    time.sleep(seconds) #stop program for a time 
+    result = int(math.sqrt(num))#finding sqrt root of number
     
-    print('The root of ' + num + 'is ' + result + 'after ' + mil + ' miliseconds')
+    print('The root of ' + num + 'is ' + result + 'after ' + mil + ' miliseconds')#printing results 
 
-#ex 5
+#ex 5 return true if all elements in the tuple are true 
 def ex5():
     tup = (True, False, True, False, True)
     false = sum(1 for i in tup if i == False)
--- lab6/dir-and-files.py
+++ lab6/dir-and-files.py
@@ -1,34 +1,34 @@
 import os
-def ex1():
-    path = str(input("Input the path: "))
-    print("Directories:")
-    for dir in os.listdir(path):
-        if os.path.isdir(os.path.join(path, dir)):
-            print(dir)
+def ex1(): 
+    path = str(input("Input the path: "))# путь к директории 
+    print("Directories:")  # Выводим заголовок "Directories:"
+    for dir in os.listdir(path): # бeгаем по всем элементам в указонной директории 
+        if os.path.isdir(os.path.join(path, dir)): # Проверяем является ли текущий элемент директорией
+            print(dir)  # Выводим имя директории
             
-    print("Files:")
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            print(file)
-    print("All:")
-    for all in os.listdir(path):
-        print(all)
+    print("Files:")  # Выводим заголовок "Files:"
+    for file in os.listdir(path): # перебираем все элементы 
+        if os.path.isfile(os.path.join(path, file)):  #проверяем является ли текущий элемент файлом
+            print(file) # выводим имя файла 
+    print("All:")  # Выводим заголовок "All:"
+    for all in os.listdir(path):  # Перебираем все элементы в указанной директории
+        print(all) # Выводим все элементы, включая и директории, и файлы
 
 def ex2():
     path = str(input('Input the path: '))
-    if os.path.exists(path):
+    if os.path.exists(path):# check is the path exists
         print('The path exists')
-        read = os.access(path, os.R_OK)
+        read = os.access(path, os.R_OK)# check is the readble
         if read:
             print('The file is readable')
         else:
             print('The file is not readable')
-        write = os.access(path, os.W_OK)
+        write = os.access(path, os.W_OK)# check is the writable 
         if write:
             print('The file is writable')
         else:
             print('The file is not writable')
-        exe = os.access(path, os.X_OK)
+        exe = os.access(path, os.X_OK)#is the executable
         if exe:
             print('The file is executable')
         else:
@@ -38,16 +38,16 @@ def ex2():
 
 def ex3():
     path = str(input('Input the path: '))
-    if os.path.exists(path):
+    if os.path.exists(path):# check is the exists
         print('The path exists')
-        path_name = os.path.basename(path)
-        path_dir = os.path.dirname(path)
+        path_name = os.path.basename(path)#get the basename of the file 
+        path_dir = os.path.dirname(path)#get directory name of the file 
         print('File name is: ', path_name)
         print('File path is: ', path_dir)
     else:
         print('The path does not exists')
       
-def ex4():
+def ex4():#number of lines in the text format  
     path = input("Input the path: ")
     with open(path, 'r') as f:
         lines = f.readlines()
@@ -58,15 +58,15 @@ def ex5():
     path = str(input("Input the path: "))
     with open(path, 'w') as file:
         list = [1, 2, 3, 4, 5, 6, 7, 8]
-        file.write('\n'.join(list))
+        file.write('\n'.join(list)) # converts each integer in the list to a string
         
-def ex6():
+def ex6(): # generate 26 text files named A.txt, B.txt, and so on
     for letter in "abcdefghijklmnopqrstuvwxyz":
         filename = letter + '.txt'
         with open(filename, 'w') as file:
             file.write(letter)
 
-def ex7():
+def ex7():# copy the contents of a file to another file
     path = str(input("Input the path: "))
     new_path = str(input("Input the new path: "))
     with open(path, 'rb') as file:
@@ -75,10 +75,10 @@ def ex7():
         new_file.write(write)
         
 
-def ex8():
+def ex8():#delete file by specified path. Before deleting check for access and  existing
     path = str(input("Input the path: "))
     if os.path.exists(path):
-        if os.access(path, os.X_OK):
+        if os.access(path, os.X_OK):# check for executability
             os.remove(path)
             print('The file have been deleted')
         else:
